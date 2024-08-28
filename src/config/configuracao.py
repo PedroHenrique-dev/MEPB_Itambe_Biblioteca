f4cd81d4b8543f50a8a7bbd89b1a1f453aa5f630
Pedro Henrique <pedrohenriquerlima1@gmail.com>
2024-08-28 20:38:09 -0300

    Refatorado configuracao.py

--- src/config/configuracao.py
+++ src/config/configuracao.py
@@ -2,26 +2,34 @@ import json
 from sys import platform
 
 
-class Configuracao():
-    def __init__(self) -> None:        
+class Configuracao:
+    def __init__(self) -> None:
+        self.nome_banco = str
+        self.conexao = str
+        self.colecoes = str
+
+        self.info = str
+        self.imagens = str
+        self.biblioteca = str
+
         self.ler_config()
         
     def ler_config(self):
-        nomeArquivo = './config.json'
+        nome_arquivo = './config.json'
         
-        with open(nomeArquivo) as f:
-            jsonArquivo = json.load(f)
+        with open(nome_arquivo) as f:
+            json_arquivo = json.load(f)
             
-        with open(nomeArquivo,'r') as arquivo:
-            diretorios = jsonArquivo['diretorios']
+        with open(nome_arquivo,'r'):
+            diretorios = json_arquivo['diretorios']
             diretorios_arquivos = diretorios['localArquivos_windows'] if platform == 'win32' else diretorios['localArquivos_linux']
             
-            self.biblioteca = diretorios_arquivos + diretorios['bibioteca']
+            self.biblioteca = diretorios_arquivos + diretorios['biblioteca']
             self.imagens = diretorios_arquivos + diretorios['imagens']
             self.info = diretorios_arquivos + diretorios['info']
             
-            self.conexao = jsonArquivo['banco_mongodb']['string_connection']
-            self.nome_banco = jsonArquivo['banco_mongodb']['string_bank']
+            self.conexao = json_arquivo['banco_mongodb']['string_connection']
+            self.nome_banco = json_arquivo['banco_mongodb']['string_bank']
             self.colecoes = {}
-            for colacao in jsonArquivo['banco_mongodb']['collections']:
-                self.colecoes[colacao] = colacao
+            for colecao in json_arquivo['banco_mongodb']['collections']:
+                self.colecoes[colecao] = colecao
